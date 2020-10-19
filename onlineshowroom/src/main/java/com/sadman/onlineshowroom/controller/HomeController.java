9327a5de7c5bc9c58b21d02e60f59cbd861a787d
Sadman Sobhan <imran110219@gmail.com>
2020-10-20 00:58:28 +0600

    spring batch added

--- onlineshowroom/src/main/java/com/sadman/onlineshowroom/controller/HomeController.java
+++ onlineshowroom/src/main/java/com/sadman/onlineshowroom/controller/HomeController.java
@@ -1,13 +1,19 @@
 package com.sadman.onlineshowroom.controller;
 
+import com.sadman.onlineshowroom.model.Category;
+import com.sadman.onlineshowroom.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
-import java.security.Principal;
+import java.util.List;
 
 @Controller
 class HomeController {
+
+    @Autowired
+    CategoryService categoryService;
 
     @GetMapping("/home")
     public String home(){
@@ -15,7 +21,9 @@ class HomeController {
     }
 
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        List<Category> categoryList = categoryService.getAllCategories();
+        model.addAttribute("categories", categoryList);
         return "index";
     }
 }
