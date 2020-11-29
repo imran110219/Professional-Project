a9bbdefbc0149a3a77d2f53b4b2eabc074a54c91
Sadman Sobhan <imran110219@gmail.com>
2020-11-30 00:31:50 +0600

    Cryptic Tool added

---
+++ Cryptographic-Tool/src/main/java/com/sadman/Test.java
@@ -0,0 +1,10 @@
+package com.sadman;
+
+/**
+ * @author Sadman
+ */
+public class Test {
+    public static void main(String[] args) {
+//         String i = Test.getClass().getResource("/view/main.fxml").getPath();
+    }
+}
